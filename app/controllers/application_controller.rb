2a0eaf717d80cf7fef37da105c4f9f7c07b30826
ZalmanB <zalmanblau@gmail.com>
2015-12-07 18:01:19 -0500

    fixing redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
 
       def authenticate_user!
         if !user_signed_in?
-          redirect_to login_path, , :notice => 'Please login to continue'
+          redirect_to '/users/sign_in', :notice => 'Please login to continue'
         end
       end
 end
